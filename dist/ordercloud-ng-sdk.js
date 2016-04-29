3d0b4926e1b3b9a3ec27fa3e4c8fad29e8616b0d
Robert <rwatt@four51.com>
2016-04-29 10:38:55 -0600

    OC-422

--- dist/ordercloud-ng-sdk.js
+++ dist/ordercloud-ng-sdk.js
@@ -39,29 +39,11 @@
         function As() {
             impersonating = true;
 
-            switch (typeof arguments[0]) {
-                case 'string':
-                    Auth().SetImpersonationToken(arguments[0]);
-                    break;
-                case 'object':
-                    CreateToken(arguments[0]);
-                    break;
-                default:
-                    break;
+            if (typeof arguments[0] == 'string') {
+                Auth().SetImpersonationToken(arguments[0]);
             }
-            return this;
 
-            function CreateToken(CredientialsObject) {
-                if (CredientialsObject.UserID && CredientialsObject.ClientID) {
-                    Users().GetAccessToken(CredientialsObject.UserID, {
-                            ClientID: CredientialsObject.ClientID,
-                            Claims: CredientialsObject.Claims ? CredientialsObject.Claims : ["FullAccess"]
-                        })
-                        .then(function(token) {
-                            Auth().SetImpersonationToken(token);
-                        });
-                }
-            }
+            return this;
         }
 
         function Auth() {
